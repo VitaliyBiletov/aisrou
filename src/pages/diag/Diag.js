8ce1c798cf075ecf6104bc5ea74c9b1cccba7206
VitaliyBiletov <vit.biletov@gmail.com>
2022-01-25 15:49:17 +0300

    Запрет изменения размеров textarea Установка animate css, fonawesome react Добавил scroll (diag) для возврата наверх с помощью кнопки Убрал скролл для body

--- src/pages/diag/Diag.js
+++ src/pages/diag/Diag.js
@@ -5,6 +5,13 @@ import Sensmotor from "./sections/sensmotor/Sensmotor";
 import Grammatic from "./sections/grammatic/Grammatic";
 import Lexis from "./sections/lexis/Lexis";
 import Progress from '../../components/progress/Progress'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleUp } from '@fortawesome/free-solid-svg-icons'
+import "animate.css/animate.css"
+
+// import * as Scroll from 'react-scroll';
+import { animateScroll as scroll } from 'react-scroll'
+
 import './style.sass'
 
 const SECTIONS = [
@@ -19,14 +26,22 @@ export default class Diag extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      activeTab: 0
+      activeTab: 0,
+      isVisibleUp: false
     }
   }
 
   componentDidMount(){
     const activeTab = !sessionStorage.getItem('activeTab') ? 0 : Number(sessionStorage.getItem('activeTab'))
     this.setState({'activeTab': activeTab})
+  }
 
+  scrollToTop = function() {
+      scroll.scrollToTop({
+          duration: 800,
+          containerId:"diag",
+          smooth: 'easeInOutCubic'
+      });
   }
 
   handleSelect = (index) => {
@@ -34,9 +49,17 @@ export default class Diag extends React.Component {
     sessionStorage.setItem('activeTab', index)
   }
 
+  handleScroll = (e) => {
+    if (e.target.scrollTop > 50){
+      this.setState({isVisibleUp: true})
+    } else {
+      this.setState({isVisibleUp: false})
+    }
+  }
+
   render() {
     return (
-      <div className="diag">
+      <div className="diag" id="diag" onScroll={this.handleScroll}>
         <Tabs className='diag__tabs' selectedIndex={this.state.activeTab} onSelect={this.handleSelect}>
           <TabList className='diag__tab-list'>
             { SECTIONS.map((s, index)=><Tab key={index} className='diag__item'>{s.tabName}</Tab>)}
@@ -48,6 +71,17 @@ export default class Diag extends React.Component {
             </TabPanel>
           ))}
         </Tabs>
+          {
+            this.state.isVisibleUp ?
+                  <button
+                      className="diag__btn diag__btn_up animate__animated animate__fadeIn"
+                      onClick={this.scrollToTop}
+                  >
+                    <FontAwesomeIcon icon={faAngleUp} size="4x"/>
+                  </button>
+              :
+            null
+          }
         <div className='diag__bottom-section'>
           <button className='diag__btn diag__btn_save'>Сохранить</button>
           <button className='diag__btn diag__btn_cancel'>Отмена</button>
